d5ea2b1048aaae0c4bddc2d1209bae058e4b009e
BELBeLaRuS <157610529+BELBeLaRuS@users.noreply.github.com>
2024-12-06 19:38:36 +0300

    Add files via upload

---
+++ index26.js
@@ -0,0 +1,137 @@
+// alert('Hello chubzik');
+// alert('Ne ispugal?');
+
+
+// let a = prompt('input your height: ');
+// alert('Your height ' + a + ' meters');
+
+
+// if(confirm('Do you wanna output index a? ')) {
+//     console.log(a);
+// }
+
+// let a = 10.654;
+// let b = `43242 - ${a}`;
+// let c = false | true;
+
+// let d = [1, 2, 45, 6, 342, 0, 'text(chezch)', {size:10, Font:12}];
+// let marker = {
+//     size: 12,
+//     radius: 10.5,
+//     draw: function(shape) {
+//         console.log(shape);
+//     }
+// };
+
+// d.forEach(function($item){
+//     console.log($item);
+
+// });
+
+// console.log(marker.size);
+// console.log(marker.radius);
+// marker.draw('треугольник');
+// marker.draw('овал');
+
+// let f = function(){
+//     alert('1231487');
+// }
+
+// f();
+
+// function myFunction(p1, p2, p3){
+//     console.log(p1 + p2 + p3);
+// }
+
+// myFunction(3, 5, 6);
+
+//1. 
+
+// let num = 10;
+
+// let result = num * num;
+
+// console.log('Result: ' + result);
+
+//2.
+
+// let num1 = 10;
+// let num2 = 20;
+
+// let result = num1 + num2;
+
+// let gg = result / 2;
+
+// console.log('Result: ' + gg);
+
+
+//3.
+
+// let num = 5;
+
+// let result = num * num;
+
+// console.log('Result: ' + result);
+
+//4.
+
+// let km = 10;
+
+// const ml = 0.621371;
+
+// let result = km * ml;
+
+// console.log('Result: ' + result);
+
+//5.
+
+// let num1 = 5;
+// let num2 = 10;
+
+// let result1 = num1 + num2;
+// let result2 = num1 - num2;
+// let result3 = num1 * num2;
+
+// console.log('Result1: ' + result1);
+// console.log('Result2: ' + result2);
+// console.log('Result3: ' + result3);
+
+//6.
+
+// let a = 5;
+// let b = 3;
+// let x = 0;
+
+// let result = (a * x) + b;
+
+// console.log('Result: ' + result);
+
+//7.
+
+// let minutes = 0.52;
+// let hours = 14;
+
+// let day = 24.00;
+
+// let result = day - hours;
+
+// let result1 = result - minutes;
+
+// console.log('Result: ' + result1);
+
+//8.
+
+// let num = 123.564634;
+
+// let result = num %  10;
+
+// console.log('Result: ' + result);
+
+//9.
+
+
+let num = (12345);
+
+let result = num(51234);
+
+console.log('Result: ' + result);
